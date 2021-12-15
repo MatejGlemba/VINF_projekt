6989bfcb51965e06ff58e09a23e7b4a1ca960c2d
Matej Glemba <32130267+MatejGlemba@users.noreply.github.com>
2021-12-15 02:32:28 +0100

    Added stats
    Signed-off-by: Matej Glemba <32130267+MatejGlemba@users.noreply.github.com>

--- vinf-pylucene.py
+++ vinf-pylucene.py
@@ -21,7 +21,7 @@ def stemming(query):
 lucene.initVM(vmargs=['-Djava.awt.headless=true'])
 #print('lucene', lucene.VERSION)
 
-if not path.exists('index'):
+if not path.exists('index') and not path.exists('index2'):
     print("Creating index...")
     store = SimpleFSDirectory(Paths.get("index"))
     analyzer = StandardAnalyzer()
@@ -35,21 +35,59 @@ if not path.exists('index'):
         for row in reader:
             doc = Document()
             #print(row)
+            doc.add(Field("id", row['id'], TextField.TYPE_STORED))
             doc.add(Field("autor", row['autor'], TextField.TYPE_STORED))
+            doc.add(Field("originalAutor", row['originalAutor'], TextField.TYPE_STORED))
             doc.add(Field("title", row['title'], TextField.TYPE_STORED))
+            doc.add(Field("originalTitle", row['originalTitle'], TextField.TYPE_STORED))
             doc.add(Field("subtitle", row['subtitle'], TextField.TYPE_STORED))
+            doc.add(Field("konspekt", row['konspekt'], TextField.TYPE_STORED))
+            doc.add(Field("podkonspekt", row['podkonspekt'], TextField.TYPE_STORED))
             doc.add(Field("abstract", row['abstract'], TextField.TYPE_STORED))
             doc.add(Field("text", row['text'], TextField.TYPE_STORED))
             doc.add(StoredField("originalText", row['originalText']))
             writer.addDocument(doc)
     writer.commit()
     writer.close()
+    
+    
+    # Create document
+    store2 = SimpleFSDirectory(Paths.get("index2"))
+    analyzer2 = StandardAnalyzer()
+    config2 = IndexWriterConfig(analyzer2)
+    config2.setOpenMode(IndexWriterConfig.OpenMode.CREATE)
+    writer2 = IndexWriter(store2, config2)
+    
+    with open('dataCsv.csv', newline='') as csvfile:
+        reader = csv.DictReader(csvfile)
+        for row in reader:
+            doc = Document()
+            #print(row)
+            doc.add(Field("transakciaID", row['transakciaID'], TextField.TYPE_STORED))
+            doc.add(Field("timestamp", row['timestamp'], TextField.TYPE_STORED))
+            doc.add(Field("operacia", row['operacia'], TextField.TYPE_STORED))
+            doc.add(Field("catId", row['catId'], TextField.TYPE_STORED))
+            doc.add(Field("cena", row['cena'], TextField.TYPE_STORED))
+            doc.add(Field("userHash", row['userHash'], TextField.TYPE_STORED))
+            doc.add(Field("psc", row['psc'], TextField.TYPE_STORED))
+            doc.add(Field("vek", row['vek'], TextField.TYPE_STORED))
+            doc.add(Field("casVypozicania", row['casVypozicania'], TextField.TYPE_STORED))
+            doc.add(Field("dlzkaTransakcie", row['dlzkaTransakcie'], TextField.TYPE_STORED))
+            doc.add(Field("pocetUpomienok", row['pocetUpomienok'], TextField.TYPE_STORED))
+            writer2.addDocument(doc)
+    writer2.commit()
+    writer2.close()
 
 # -------- SEARCHING --------
 print("Searching...")
 directory = SimpleFSDirectory(Paths.get("index"))
 searcher = IndexSearcher(DirectoryReader.open(directory))
+
+directory = SimpleFSDirectory(Paths.get("index2"))
+searcher2 = IndexSearcher(DirectoryReader.open(directory))
+
 analyzer = StandardAnalyzer()
+
 
 inputKeys = sys.argv[1].strip()
 top = sys.argv[2]
@@ -89,7 +127,159 @@ query = QueryParser("text", analyzer).parse(inputKeys)
 scoreDocs = searcher.search(query, int(top)).scoreDocs
 
 print("%s total matching documents." % len(scoreDocs))
-for scoreDoc in scoreDocs:
-    print("---------------------------------------")
-    doc = searcher.doc(scoreDoc.doc)
-    print('Score: ', scoreDoc, 'Data: ', doc.get('originalText'))
+option = ''
+if len(scoreDocs) > 0:
+    print("--------------------------------")
+    print("1 - operacie podla autora")
+    print("2 - konspekty podla autora")
+    print("3 - okres podla autora")
+    print("4 - vekova skupina podla autora")
+    print("0 - koniec")
+    print("--------------------------------")
+    option = input()
+
+    if option == 1:
+        if not path.exists('indexOperacie'):
+            print("Creating indexOperacie...")
+            storeOP = SimpleFSDirectory(Paths.get("indexOperacie"))
+            analyzerOP = StandardAnalyzer()
+            configOP = IndexWriterConfig(analyzerOP)
+            configOP.setOpenMode(IndexWriterConfig.OpenMode.CREATE)
+            writerOP = IndexWriter(storeOP, configOP)
+
+            # Create document
+            with open('operacie.csv', newline='') as csvfile:
+                reader = csv.DictReader(csvfile)
+                for row in reader:
+                    doc = Document()
+                    #print(row)
+                    doc.add(Field("originalAutor", row['originalAutor'], TextField.TYPE_STORED))
+                    doc.add(Field("operacia", row['operacia'], TextField.TYPE_STORED))
+                    doc.add(Field("count", row['count'], TextField.TYPE_STORED))
+                    writerOP.addDocument(doc)
+            writerOP.commit()
+            writerOP.close()
+        directoryOP = SimpleFSDirectory(Paths.get("indexOperacie"))
+        searcherOP = IndexSearcher(DirectoryReader.open(directoryOP))
+        analyzerOP = StandardAnalyzer()
+        
+        for scoreDoc in scoreDocs:
+            print("---------------------------------------")
+            doc = searcher.doc(scoreDoc.doc)
+            print('Score: ', scoreDoc, 'Data: ', doc.get('originalText'))
+            print("----------------STATS------------------")
+            queryOP = QueryParser("originalAutor", analyzerOP).parse(doc.get('originalAutor'))
+            scoreDocsOP = searcherOP.search(queryOP, 15).scoreDocs
+            print("%s total matching OP documents." % len(scoreDocsOP))
+            for scoreDocOP in scoreDocsOP: 
+                print(scoreDocOP)
+    elif option == 2:
+        if not path.exists('indexKonspekt'):
+            print("Creating indexKonspekt...")
+            storeKON = SimpleFSDirectory(Paths.get("indexKonspekt"))
+            analyzerKON = StandardAnalyzer()
+            configKON = IndexWriterConfig(analyzerKON)
+            configKON.setOpenMode(IndexWriterConfig.OpenMode.CREATE)
+            writerKON = IndexWriter(storeKON, configKON)
+
+            # Create document
+            with open('konspekt.csv', newline='') as csvfile:
+                reader = csv.DictReader(csvfile)
+                for row in reader:
+                    doc = Document()
+                    #print(row)
+                    doc.add(Field("originalAutor", row['originalAutor'], TextField.TYPE_STORED))
+                    doc.add(Field("konspekt", row['konspekt'], TextField.TYPE_STORED))
+                    doc.add(Field("count", row['count'], TextField.TYPE_STORED))
+                    writerKON.addDocument(doc)
+            writerKON.commit()
+            writerKON.close()
+        directoryKON = SimpleFSDirectory(Paths.get("indexKonspekt"))
+        searcherKON = IndexSearcher(DirectoryReader.open(directoryKON))
+        analyzerKON = StandardAnalyzer()
+        
+        for scoreDoc in scoreDocs:
+            print("---------------------------------------")
+            doc = searcher.doc(scoreDoc.doc)
+            print('Score: ', scoreDoc, 'Data: ', doc.get('originalText'))
+            print("----------------STATS------------------")
+            queryKON = QueryParser("originalAutor", analyzerKON).parse(doc.get('originalAutor'))
+            scoreDocsKON = searcherKON.search(queryKON, 15).scoreDocs
+            print("%s total matching OP documents." % len(scoreDocsKON))
+            for scoreDocKON in scoreDocsKON: 
+                print(scoreDocKON)
+    elif option == 3:
+        if not path.exists('indexOkres'):
+            print("Creating indexOkres...")
+            storeOKR = SimpleFSDirectory(Paths.get("indexOkres"))
+            analyzerOKR = StandardAnalyzer()
+            configOKR = IndexWriterConfig(analyzerOKR)
+            configOKR.setOpenMode(IndexWriterConfig.OpenMode.CREATE)
+            writerOKR = IndexWriter(storeOKR, configOKR)
+
+            # Create document
+            with open('psc.csv', newline='') as csvfile:
+                reader = csv.DictReader(csvfile)
+                for row in reader:
+                    doc = Document()
+                    #print(row)
+                    doc.add(Field("originalAutor", row['originalAutor'], TextField.TYPE_STORED))
+                    doc.add(Field("psc", row['psc'], TextField.TYPE_STORED))
+                    doc.add(Field("count", row['count'], TextField.TYPE_STORED))
+                    writerOKR.addDocument(doc)
+            writerOKR.commit()
+            writerOKR.close()
+        directoryOKR = SimpleFSDirectory(Paths.get("indexOkres"))
+        searcherOKR = IndexSearcher(DirectoryReader.open(directoryOKR))
+        analyzerOKR = StandardAnalyzer()
+           
+        for scoreDoc in scoreDocs:
+            print("---------------------------------------")
+            doc = searcher.doc(scoreDoc.doc)
+            print('Score: ', scoreDoc, 'Data: ', doc.get('originalText'))
+            print("----------------STATS------------------")
+            queryOKR = QueryParser("originalAutor", analyzerOKR).parse(doc.get('originalAutor'))
+            scoreDocsOKR = searcherOKR.search(queryOKR, 15).scoreDocs
+            print("%s total matching OP documents." % len(scoreDocsOKR))
+            for scoreDocOKR in scoreDocsOKR: 
+                print(scoreDocOKR)
+    elif option == 4:
+        if not path.exists('indexVek'):
+            print("Creating indexVek...")
+            storeVEK = SimpleFSDirectory(Paths.get("indexVek"))
+            analyzerVEK = StandardAnalyzer()
+            configVEK = IndexWriterConfig(analyzerVEK)
+            configVEK.setOpenMode(IndexWriterConfig.OpenMode.CREATE)
+            writerVEK = IndexWriter(storeVEK, configVEK)
+
+            # Create document
+            with open('vekSkupina.csv', newline='') as csvfile:
+                reader = csv.DictReader(csvfile)
+                for row in reader:
+                    doc = Document()
+                    #print(row)
+                    doc.add(Field("originalAutor", row['originalAutor'], TextField.TYPE_STORED))
+                    doc.add(Field("vekSkupina", row['vekSkupina'], TextField.TYPE_STORED))
+                    doc.add(Field("count", row['count'], TextField.TYPE_STORED))
+                    writerVEK.addDocument(doc)
+            writerVEK.commit()
+            writerVEK.close()
+        directoryVEK = SimpleFSDirectory(Paths.get("indexVek"))
+        searcherVEK = IndexSearcher(DirectoryReader.open(directoryVEK))
+        analyzerVEK = StandardAnalyzer()
+           
+        for scoreDoc in scoreDocs:
+            print("---------------------------------------")
+            doc = searcher.doc(scoreDoc.doc)
+            print('Score: ', scoreDoc, 'Data: ', doc.get('originalText'))
+            print("----------------STATS------------------")
+            queryVEK = QueryParser("originalAutor", analyzerVEK).parse(doc.get('originalAutor'))
+            scoreDocsVEK = searcherVEK.search(queryVEK, 15).scoreDocs
+            print("%s total matching OP documents." % len(scoreDocsVEK))
+            for scoreDocVEK in scoreDocsVEK: 
+                print(scoreDocVEK)
+    else:
+        for scoreDoc in scoreDocs:
+            print("---------------------------------------")
+            doc = searcher.doc(scoreDoc.doc)
+            print('Score: ', scoreDoc, 'Data: ', doc.get('originalText'))
--- vinf-spark.py
+++ vinf-spark.py
@@ -2,20 +2,139 @@
 # encoding=utf8  
 
 import sys
+from pyspark import rdd
 from pyspark.sql import SparkSession
 from pyspark.sql.types import StringType, StructField, StructType
+from pyspark.sql.functions import col, asc, desc
 import unicodedata
 import regex
 import cze_stemmer
 
-extract_record_info_schema = StructType([
+schema_for_xml = StructType([
+    StructField("id", StringType(), True),
     StructField("autor", StringType(), True),
+    StructField("originalAutor", StringType(), True),
     StructField("title", StringType(), True),
+    StructField("originalTitle", StringType(), True),
     StructField("subtitle", StringType(), True),
     StructField("abstract", StringType(), True),
+    StructField("konspekt", StringType(), True),
+    StructField("podkonspekt", StringType(), True),
     StructField("text", StringType(), True),
     StructField("originalText", StringType(), True)
 ])
+
+schema_for_csv = StructType([
+    StructField("transakciaID", StringType(), True),
+    StructField("timestamp", StringType(), True),
+    StructField("operacia", StringType(), True),
+    StructField("catId", StringType(), True),
+    StructField("cena", StringType(), True),
+    StructField("userHash", StringType(), True),
+    StructField("psc", StringType(), True),
+    StructField("vek", StringType(), True),
+    StructField("vekSkupina", StringType(), True),
+    StructField("casVypozicania", StringType(), True),
+    StructField("dlzkaTransakcie", StringType(), True),
+    StructField("pocetUpomienok", StringType(), True)
+])
+
+konspektValues = {
+    1 : "antropologia, etnografia",
+    2 : "biologicke vedy",
+    3 : "divadlo, film, tanec",
+    4 : "ekonomicke vedy, obchod",
+    5 : "filozofia a nabozenstvo",
+    6 : "fyzika a pribuzne vedy",
+    7 : "geografia a geologia",
+    8 : "historia",
+    9 : "hudba",
+    10 : "chemia",
+    11 : "jazyk, lingvistika",
+    12 : "knihovnictvo, informatika",
+    13 : "matematika",
+    14 : "lekarstvo",
+    15 : "politicke vedy",
+    16 : "pravo",
+    17 : "psychologia",
+    18 : "sociologia",
+    19 : "technika, technologie",
+    20 : "telesna vychova a sport",
+    21 : "umenie, architektura",
+    22 : "vychova a vzdelavanie",
+    23 : "vypoctova technika",
+    24 : "polnohospodarstvo",
+    25 : "beletria",
+    26 : "literatura pre deti a mladez"
+}
+
+operacie = {
+    "V" : "vypozicka",
+    "N" : "navrat",
+    "P" : "prolongacia",
+    "U1" : "upomienka-1",
+    "U2" : "upomienka-2",
+    "U3" : "upomienka-3",
+    "U4" : "upomienka-4",
+    "U5" : "upomienka-5",
+    "U6" : "upomienka-6",
+    "U7" : "upomienka-7",
+    "U8" : "upomienka-8",
+    "U9" : "upomienka-9",
+    "Z" : "ziadanka",
+    "H" : "nevyziadana ziadanka",
+    "R" : "rezervacia",
+    "L" : "platba",
+    "E" : "presun",
+    "S" : "statistika",
+    "W" : "volny vyber",
+    "M" : "mvs",
+    "K" : "kopia",
+    "G" : "e-vypozicka"
+}
+
+pscValues = {
+    37367 : "borek",
+    37312 : "borovany",
+    37382 : "borsov nad vltavou",
+    37362 : "chotycany",
+    37304 : "chrastany",
+    37316 : "dobra voda u ceskych budejovic",
+    37365 : "dolni bukovsko",
+    37008 : "dubravice",
+    37006 : "dubicne",
+    37364 : "dynin",
+    37348 : "divcice",
+    37351 : "driten",
+    37341 : "hluboka nad vltavou",
+    37335 : "horni stropnice",
+    37372 : "hvozdec",
+    37384 : "jankov",
+    37332 : "jilovice",
+    37314 : "komarice",
+    37311 : "ledenice",
+    37322 : "locenice",
+    37363 : "mazelov",
+    37901 : "mladosovice",
+    37349 : "mydlovary",
+    37333 : "nove hrady",
+    37331 : "olesnice",
+    37350 : "olesnik",
+    37346 : "pistin",
+    37007 : "plav",
+    37371 : "rudolfov",
+    37321 : "slavve",
+    37323 : "svaty jan nad malsi",
+    37305 : "temelin",
+    37401 : "trhove sviny",
+    37501 : "tyn nad vltavou",
+    37344 : "zliv",
+    37010 : "usilne",
+    39165 : "cenkov u bechyne",
+    37001 : "ceske budejovice",
+    37373 : "stepanovice",
+    37366 : "zimutice"
+}
 
 def load_stopwords():
     global stopWords
@@ -44,16 +163,107 @@ def removeStopWords(string):
             new_list_of_terms.append(term)
     list_of_terms = None
     return new_list_of_terms
-    
-def process(rdd):
+ 
+def processKonspekt(konspekt):
+    if konspekt:
+        return konspektValues[int(konspekt)]
+    return ''
+
+def processOperation(op):
+    if op:
+        return operacie[op]
+    return ''
+
+def processPsc(psc):
+    if psc:
+        psc = regex.sub("\s", "", psc)
+        try:
+            psc = int(psc)
+        except ValueError:
+            return ''
+        if psc in pscValues:
+            return pscValues[int(psc)]
+    return ''
+
+def processCatId(catId):
+    if catId:
+        return regex.split("\*", catId)[1]
+    return catId
+
+def processCena(cena):
+    if cena:
+        cena = regex.sub("Kč ", "", cena)
+        cena = regex.sub("\.", ",", cena)
+    return str(cena)
+
+def processDate(date):
+    if date:
+        return date[0:4] + "-" + date[4:6] + "-" + date[6:8]# + "-" + date[8:10] + ":" + date[10:12]
+    return date
+
+def processVek(vek):
+    if vek and vek.isdigit():
+        vek = int(vek)
+        if vek < 20:
+            return 1
+        elif vek < 30:
+            return 2
+        elif vek < 40:
+            return 3
+        elif vek < 50:
+            return 4
+        elif vek < 60:
+            return 5
+        elif vek < 70:
+            return 6
+        elif vek < 100:
+            return 7
+        else: 
+            return 0
+    return 0
+
+def processCsv(row):
+    resultsArray = []
+    results = {}
+    results['transakciaID'] = row['ArlID']
+    results['timestamp'] = processDate(str(row['tcreate'])) 
+    results['operacia'] = processOperation(str(row['op']))
+    results['catId'] = processCatId(str(row['catId']))
+    results['cena'] = processCena(str(row['Tcat_T020c']))
+    results['userHash'] = row['userHash']
+    results['psc'] = processPsc(row['psc'])
+    results['vek'] = row['vek']
+    results['vekSkupina'] = processVek(row['vek'])
+    results['casVypozicania'] = row['casVypujceni']
+    results['dlzkaTransakcie'] = row['DelkaTransakce']
+    results['pocetUpomienok'] = row['pocetUpominek']
+    resultsArray.append(results)
+    return resultsArray
+
+def processAutor(autor):
+    if autor:
+        splitArr = regex.split(",", autor)
+        splitArr = list(reversed(splitArr))
+        autor = ''
+        for s in splitArr:
+            if s:
+                autor += s + ' '
+        #return str(splitArr)
+    return autor.strip()
+            
+def process(controlfield, datafield):
     """
     Read the xml string from rdd, parse and extract the elements,
     then return a list of list.
     """
     resultsArray = []
-    rdd_text = str(rdd)
+    controlfield_text = str(controlfield)
+    controlfield_text = regex.search("Row\(_VALUE='(?P<id>[^']*)', _tag=1\)", controlfield_text)
+    if controlfield_text:
+        controlfield_text = controlfield_text.group('id')
+    rdd_text = str(datafield)
     rdd_text = regex.sub("u'", "'", rdd_text)
-    data = regex.finditer("Row\(_ind1='1', _ind2='0', _tag='245', subfield=\[Row\(_VALUE='(?P<title>[^']*)', _code='a'\)(, Row\(_VALUE='(?P<subtitle>[^']*)', _code='b'\))?(, Row\(_VALUE='(?P<autor>[^']*)', _code='c'\))?](.+?_tag='520', subfield=\[(Row\(_VALUE='(?P<abstract>[^']*)', _code='a'\))?)?", rdd_text)
+    data = regex.finditer("(Row\(_ind1='1', _ind2=' ', _tag='100', subfield=\[(Row\(_VALUE='(?P<autor>[^']*)', _code='a'\))?)(.+?Row\(_ind1='1', _ind2='0', _tag='245', subfield=\[Row\(_VALUE='(?P<title>[^']*)', _code='a'\)(, Row\(_VALUE='(?P<subtitle>[^']*)', _code='b'\))?)?(.+?_tag='520', subfield=\[(Row\(_VALUE='(?P<abstract>[^']*)', _code='a'\))?)?(.+?_tag='C72', subfield=\[(Row\(_VALUE='.+?', _code='a'\))?(, Row\(_VALUE='(?P<konspekt>\d+)', _code='9'\))?(, Row\(_VALUE='(?P<podkonspekt>[^']*)', _code='x'\))?\]\))?", rdd_text)
     for d in data:
         results = {}
         data_dict = d.groupdict()
@@ -68,12 +278,23 @@ def process(rdd):
             autor = ""
         abstract = data_dict['abstract']
         if abstract is None:
-            abstract = ""  
+            abstract = "" 
+        konspekt = data_dict['konspekt']
+        if konspekt is None:
+            konspekt = ""
+        podkonspekt = data_dict['podkonspekt']
+        if podkonspekt is None:
+            podkonspekt = ""     
         results['originalText'] = "Autor: " + autor + ", Title: " + title + ", Subtitle: " + subtitle + ", Abstract: " + abstract 
-        results['autor'] = processData(autor)
+        results['id'] = controlfield_text
+        results['autor'] = processData(processAutor(autor))
+        results['originalAutor'] = processAutor(autor)
         results['title'] = processData(title)
+        results['originalTitle'] = title
         results['subtitle'] = processData(subtitle)
         results['abstract'] = processData(abstract)
+        results['konspekt'] = processKonspekt(konspekt)
+        results['podkonspekt'] = podkonspekt
         results['text'] = results['autor'] + results['title'] + results['subtitle'] + results['abstract']
         resultsArray.append(results)
     return resultsArray
@@ -92,33 +313,126 @@ def processData(data):
 
 def runProgram():
     load_stopwords()
-    inputFile = sys.argv[1]
-    if not inputFile:
-        inputFile = "data-short.xml"
-        
+    inputFileXml = sys.argv[1]
+    if not inputFileXml:
+        inputFileXml = "data-short.xml"
+    inputFileCsv = sys.argv[3]
+    if not inputFileCsv:
+        inputFileCsv = "transactions-short.csv"
+    
     spark = SparkSession.builder.getOrCreate()
+    #-------------- spracovanie XML -------------#
     df = spark.read.format("com.databricks.spark.xml")\
         .options(rowTag='record', rootTag='collection', encode='utf-8')\
-        .load(inputFile)
+        .load(inputFileXml)
     #print(df.take(2))
     df.printSchema()
-    selected = df.select("datafield")\
+    selected = df.select("datafield", "controlfield")\
         .dropna(how='any')\
         .rdd\
-        .flatMap(lambda row: process(row['datafield']))
+        .flatMap(lambda row: process(row['controlfield'], row['datafield']))
     #print(selected.collect())
-    df2 = spark.createDataFrame(data=selected, schema = extract_record_info_schema)
+    df2 = spark.createDataFrame(data=selected, schema = schema_for_xml)
     df2.printSchema()
-    #df2.show(3, False)
+    df2.show(5, False)
+    #-------------- spracovanie CSV -------------# 
+    dfcsv = spark.read.format('com.databricks.spark.csv').option("header", True)\
+        .csv(inputFileCsv)
+    dfcsv.printSchema()
+    selectedCsv = dfcsv.select("ArlID","tcreate","op","catId","Tcat_T020c","userHash","psc", "vek","casVypujceni","DelkaTransakce","pocetUpominek")\
+        .dropna(how='any')\
+        .rdd\
+        .flatMap(lambda row: processCsv(row))
+    #print(selectedCsv.collect())        
+    dfcsv2 = spark.createDataFrame(data=selectedCsv, schema=schema_for_csv)
+    dfcsv2.printSchema()
+    #-------------- vytvaranie statistickych tabuliek -------------#
+    tempDf2 = df2
+    tempCsv2 = dfcsv2
+    joindf = tempDf2.join(tempCsv2,tempDf2.id ==  tempCsv2.catId,"inner")\
+        .show(truncate=False)
     
-    outputFile = sys.argv[2]
-    if not outputFile:
-        outputFile = "data"
+    tempDf2 = df2
+    tempCsv2 = dfcsv2
+    autorOperaciaDF = tempDf2.join(tempCsv2,tempDf2.id ==  tempCsv2.catId,"inner")\
+        .groupBy("originalAutor", "operacia") \
+        .count().alias("count")\
+        .orderBy(col("count").desc(),col("originalAutor").desc())\
+        .show(truncate=False)
+    
+    tempDf2 = df2
+    tempCsv2 = dfcsv2
+        
+    autorKonspektDF = tempDf2.join(tempCsv2,tempDf2.id ==  tempCsv2.catId,"inner")\
+        .groupBy("originalAutor", "konspekt") \
+        .count().alias("count")\
+        .orderBy(col("count").desc(),col("originalAutor").desc())\
+        .show(truncate=False)
+        
+    tempDf2 = df2
+    tempCsv2 = dfcsv2
+        
+    autorPscDF = tempDf2.join(tempCsv2,tempDf2.id ==  tempCsv2.catId,"inner")\
+        .groupBy("originalAutor", "psc") \
+        .count().alias("count")\
+        .orderBy(col("count").desc(),col("originalAutor").desc())\
+        .show(truncate=False)
+        
+    tempDf2 = df2
+    tempCsv2 = dfcsv2
+    
+    autorVekSkupinaDF = tempDf2.join(tempCsv2,tempDf2.id ==  tempCsv2.catId,"inner")\
+        .groupBy("originalAutor", "vekSkupina") \
+        .count().alias("count")\
+        .orderBy(col("count").desc(),col("originalAutor").desc())\
+        .show(truncate=False)
+        
+    tempDf2 = df2
+    tempCsv2 = dfcsv2
+    
+    #joindf5 = tempDf2.join(tempCsv2,tempDf2.id ==  tempCsv2.catId,"inner")\
+    #    .groupBy("originalAutor", "transakciaID", "operacia") \
+    #    .count().alias("count")\
+    #    .orderBy(col("count").desc(),col("transakciaID").desc())\
+    #    .show(truncate=False)
+    
+    
+    # ------------ zapis dat do csv -------------------------------#
+    outputFileXml = sys.argv[2]
+    if not outputFileXml:
+        outputFileXml = "dataXml"
+        
+    outputFileCsv = sys.argv[4]
+    if not outputFileCsv:
+        outputFileCsv = "dataCsv"
     
     df2.write.option("delimiter", ",")\
         .option("header", "true")\
         .format('com.databricks.spark.csv')\
-        .save(outputFile)
+        .save(outputFileXml)
+
+    dfcsv2.write.option("delimiter", ",")\
+        .option("header", "true")\
+        .format('com.databricks.spark.csv')\
+        .save(outputFileCsv)
+    
+    autorKonspektDF.write.option("delimiter", ",")\
+        .option("header", "true")\
+        .format('com.databricks.spark.csv')\
+        .save("konspekt")
+    autorOperaciaDF.write.option("delimiter", ",")\
+        .option("header", "true")\
+        .format('com.databricks.spark.csv')\
+        .save("operacie")
+    autorPscDF.write.option("delimiter", ",")\
+        .option("header", "true")\
+        .format('com.databricks.spark.csv')\
+        .save("psc")
+    autorVekSkupinaDF.write.option("delimiter", ",")\
+        .option("header", "true")\
+        .format('com.databricks.spark.csv')\
+        .save("vekSkupina")
     
 # run program
-runProgram()
+if __name__ == '__main__':
+    runProgram()
